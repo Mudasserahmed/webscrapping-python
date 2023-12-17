ab80cb5d12ad33c6a2d10e8b0dbe249076de3121
Mudasser ahmed <98097744+Mudasserahmed@users.noreply.github.com>
2023-12-17 14:56:45 +0500

    some changes

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 # url for scrapping
-url = "https://www.freecodecamp.org/"
+url = "https://www.codewithharry.com/"
 
 req = requests.get(url)
 # get the html
 html = req.content
 # parse the html
-soup = BeautifulSoup(html,"html.parser")
+soup = BeautifulSoup(html, "html.parser")
 # get the content from html
 title = soup.title
 # commonly used types of objects:
@@ -15,4 +15,28 @@ title = soup.title
 # 2. NavigableString
 # 3. BeautifulSoup
 # 4. comment
-print(type(title))
+# print(type(title))
+
+# paras = soup.find_all("p")
+# print(paras)
+
+print(soup.find('a').get_text())
+
+anchors = soup.find_all("a")
+print(anchors)
+for link in anchors:
+    if link != "#":
+      print(link.get("href"))
+
+# data types in python
+# x = "name" string
+# x = ["a","b","c"] tuple
+# x = range(9)  range
+# x = {"name","john","age"} set
+# x = {"name":"john", "age" : 22} dict
+# x = frozenset({"apple","banana","cherry"})  frozenset
+# x= true
+# x = b"hello" bytes 
+#  x = bytearray(5) bytearray
+# x = memoryview(bytes(5)) memoryview 
+# x= none Nonetype
